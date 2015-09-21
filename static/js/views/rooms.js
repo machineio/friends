7a3d1aea15da09022ea349a7ecce6152bd7a51f9
Jean Chassoul <chassoul@gmail.com>
2015-09-21 13:55:02 -0600

    go gtony-end

---
+++ static/js/views/rooms.js
@@ -0,0 +1,33 @@
+fun.views.rooms = Backbone.View.extend({
+
+    /**
+    * Bind the event functions to the different HTML elements
+    */
+    events: {
+        
+    },
+    
+    /**
+    * Class constructor
+    */
+    initialize: function(options){
+        fun.containers.rooms = this.$el;
+    },
+
+    /**
+    * Render view
+    */
+    render: function(){
+        'use strict';
+        //view cache
+        var view = this,
+            template;
+
+        console.log('render rooms view');
+
+        template = _.template(fun.utils.getTemplate(fun.conf.templates.rooms));
+
+        this.$el.html(template);
+    }
+
+});
