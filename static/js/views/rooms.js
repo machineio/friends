70ddef949d7883641ec2a8455521f4f2a011bc60
Jean Chassoul <chassoul@gmail.com>
2015-10-01 13:29:51 -0600

    dont

--- static/js/views/rooms.js
+++ static/js/views/rooms.js
@@ -4,7 +4,7 @@ fun.views.rooms = Backbone.View.extend({
     * Bind the event functions to the different HTML elements
     */
     events: {
-        
+        'click #rooms-booknow-btn': 'booknow'
     },
     
     /**
@@ -29,6 +29,59 @@ fun.views.rooms = Backbone.View.extend({
 
         this.$el.html(template);
         this.$el.removeClass("hide").addClass("show");
+    },
+
+    booknow: function(event){
+        // Render view function
+        'use strict';
+        var email,
+            room,
+            checkin,
+            checkout,
+            guests,
+            stufa,
+            details,
+            taskPayload,
+            task,
+            view = this;
+        event.preventDefault();
+
+        email = this.$('#email').val();
+        room  = this.$('#room').val();
+        checkin = this.$('#checkin').val();
+        checkout = this.$('#checkout').val();
+        guests = this.$('#guests').val();
+
+        console.log(email, room, checkin, checkout, guests);
+
+        stufa = {
+            'room':room,
+            'checkin':checkin,
+            'checkout':checkout,
+            'guests':guests
+        };
+
+        details = JSON.stringify(stufa);
+
+        taskPayload = {
+            first_name: 'Random',
+            last_name: 'Funster',
+            title: 'new reservation',
+            description: details,
+            label: 'QTuanis Reservation',
+            email: email,
+        };
+
+        task = new fun.models.Task(taskPayload);
+        task.save();
+
+        console.log('wut? on booknow');
+
+        // clean stuff on qtuanis
+        view.$('#email').val('');
+        view.$('#checkin').val('');
+        view.$('#checkout').val('');
+        view.$('#guests').val('');
     }
 
 });
